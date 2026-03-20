5fcbab45f83a643281bb50cf881d0639b315f2c4
Nguyen Huy <huynguyen2416@gmail.com>
2026-03-20 10:35:59 +0700

    test

--- Admin/index.php
+++ Admin/index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Truyện Hay - Đọc truyện tranh online</title>
+    <title>Truyện Hay - Quản lý web</title>
     <link rel="stylesheet" href="style.css">
     <!-- FontAwesome cho các icon (dùng CDN) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
