a2b8f4e44ff7ee68f5db609a2a435fbf8cad4792
Phuoc Vo Huu <kurumitokisaki15111006@gmail.com>
2026-03-21 21:43:22 +0700

    file-update

---
+++ Customer/login.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Đăng nhập</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div style="position: relative;">
+        <img src="zzz.png" class="cat-login">
+    <div class="login-card">
+        <h2>Đăng nhập</h2>
+        <form>
+            <div class="form-group">
+                <label>Email/tên đăng nhập</label>
+                <input type="text" placeholder="name@example.com">
+            </div>
+            <div class="form-group">
+                <label>Mật khẩu</label>
+                <input type="password" placeholder="••••••••">
+            </div>
+            <a href="index.php" style="text-decoration: none;">
+                <button type="button" class="btn-login">Đăng nhập</button>
+            </a>
+            <p style="font-size: 12px; color: #666; margin: 20px 0;">HOẶC</p>
+            <a href="register.php" style="text-decoration:none ;">
+                <button type="button" class="btn-register" href="register.php">Đăng ký</button>
+            </a>
+        </form>
+    </div>
+</div>
+</body>
+</html>
