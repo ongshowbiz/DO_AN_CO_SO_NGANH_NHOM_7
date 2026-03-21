850dcf38d8b252df4692639e9fcf87ed62436282
Phuoc Vo Huu <kurumitokisaki15111006@gmail.com>
2026-03-21 22:24:33 +0700

    chinhsua

--- Admin/register.php
+++ Admin/register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Đăng Kí</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-login.css">
 </head>
 <body>
     <div style="position: relative;">
