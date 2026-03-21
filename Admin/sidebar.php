45f5f25581a6978db2f0a41b79cac54917661dd2
Phuoc Vo Huu <kurumitokisaki15111006@gmail.com>
2026-03-21 23:45:45 +0700

    fix

---
+++ Admin/sidebar.php
@@ -0,0 +1,103 @@
+
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <a href="index.php" class="brand-link">
+    <img src="assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">TRANG QUẢN TRỊ</span>
+  </a>
+
+  <div class="sidebar">
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+
+          <li class="nav-header">QUẢN LÝ TRUYỆN</li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-box-open"></i>
+              <p>
+                Quản lý truyện
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="index.php?page=film-list" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Danh sách truyện</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="index.php?page=category-list" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Quản lý Thể Loại</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="index.php?page=film-list" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Số lượng truyện</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="index.php?page=film-list" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Danh hàng tồn</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-header">QUẢN LÝ HỆ THỐNG</li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-users"></i>
+              <p>
+                Quản lý User
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="index.php?page=user-list" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Danh sách User</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="index.php?page=role-list" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Quản lý Vai trò</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+           <li class="nav-header">QUẢN LÝ COMMENT</li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-comment"></i>
+              <p>
+                Quản lý Comment 
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="index.php?page=user-list" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>bình luận của truyện</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="index.php?page=role-list" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>bình luận từng chap của truyện</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+    </nav>
+  </div>
